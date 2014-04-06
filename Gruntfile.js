9517af94a909d8a13d9091123bb5f8eb341bba7d
Donald Martinez <badongers@gmail.com>
2014-04-06 21:14:12 +1000

    Initial build commit - still untested

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,15 +8,43 @@ module.exports = function(grunt) {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
             build: {
-                src: 'src/**/*.js',
+                src: [
+                    'src/core.js',
+                    'src/events/signal.js',
+                    'src/events/eventchannel.js',
+                    'src/net/xhr.js',
+                    'src/delegates/document.js',
+                    'src/delegates/module.js',
+                    'src/utils/raf.js'
+                ],
                 dest: '<%= pkg.name %>.min.js'
             }
+        },
+        concat: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dist: {
+                src: [
+                    'src/core.js',
+                    'src/events/signal.js',
+                    'src/events/eventchannel.js',
+                    'src/net/xhr.js',
+                    'src/delegates/document.js',
+                    'src/delegates/module.js',
+                    'src/utils/raf.js'
+                ],
+                dest: 'dist/core-concat.js'
+            }
         }
+
     });
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // Default task(s).
     grunt.registerTask('build', ['uglify']);
+    grunt.registerTask('concat', ['concat']);
 
 };
