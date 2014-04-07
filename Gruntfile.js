753079e181e47bd1d97eba8b05cffd7d250c8dd6
badongers <badongers@gmail.com>
2014-04-08 07:05:16 +1000

    In progress - removal of jquery dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,44 +7,33 @@ module.exports = function(grunt) {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
-            build: {
-                src: [
-                    'src/core.js',
-                    'src/events/signal.js',
-                    'src/events/eventchannel.js',
-                    'src/net/xhr.js',
-                    'src/delegates/document.js',
-                    'src/delegates/module.js',
-                    'src/utils/raf.js'
-                ],
-                dest: '<%= pkg.name %>.min.js'
-            }
-        },
-        concat: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            dist: {
-                src: [
-                    'src/core.js',
-                    'src/events/signal.js',
-                    'src/events/eventchannel.js',
-                    'src/net/xhr.js',
-                    'src/delegates/document.js',
-                    'src/delegates/module.js',
-                    'src/utils/raf.js'
-                ],
-                dest: 'dist/core-concat.js'
-            }
+
+                my_target:{
+                    files:{
+                        '<%= pkg.name %>.min.js':[
+                            'src/core.js',
+                            'src/events/signal.js',
+                            'src/events/eventchannel.js',
+                            'src/net/xhr.js',
+                            'src/delegates/document.js',
+                            'src/delegates/module.js',
+                            'src/utils/raf.js'
+                        ],
+                        '<%= pkg.name %>.plugins.min.js':[
+                            'addons/**/*.js'
+                        ]
+                    }
+                }
+
         }
 
     });
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+
 
     // Default task(s).
     grunt.registerTask('build', ['uglify']);
-    grunt.registerTask('concat', ['concat']);
+
 
 };
