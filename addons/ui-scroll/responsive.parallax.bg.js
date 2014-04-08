d01a842eb2428300c5ae8a3525943d78c22a883b
badongers <badongers@gmail.com>
2014-04-08 12:45:49 +1000

    In progress - removal of jquery dependencies

--- addons/ui-scroll/responsive.parallax.bg.js
+++ addons/ui-scroll/responsive.parallax.bg.js
@@ -15,7 +15,6 @@
     };
     proto.construct = function(opts){
         __super__.construct.call(this, opts);
-
     };
     proto.loadImages = function(){
         this.midW = 768;
@@ -23,71 +22,77 @@
         var len = this.img.length;
         var i = -1;
         var loaded = 0;
-
         var fakes = [];
-        while(i++ < len){
-            fakes[i] = $("<img/>").css({
-                position:"absolute",
-                left:0,
-                top:0,
-                visibility:"hidden"
-
-            });
-            $("body").append(fakes[i]);
-            fakes[i].attr("src", $(this.img[i]).attr("data-src"));
-            fakes[i].on("load", (function(xx){
+        while(i++ < len-1){
+            var img = document.createElement("img");
+            img.style.position = "absolute";
+            img.style.left = 0+"px";
+            img.style.top = 0+"px";
+            img.style.visibility = "hidden";
+            fakes[i] = img;
+            document.body.appendChild(img);
+            img.setAttribute("src", this.img[i].getAttribute("data-src"));
+            img.onload = (function(xx){
                 loaded++;
-                $(this.img[xx]).width(fakes[xx].width());
-                $(this.img[xx]).height(fakes[xx].height());
-                fakes[xx].remove();
+                this.img[xx].style.width = core.rect(fakes[xx]).width+"px";
+                this.img[xx].style.height = core.rect(fakes[xx]).height+"px";
+                document.body.removeChild(fakes[xx]);
                 if(loaded == len){
                     fakes = null;
                     this.startHandlingImages();
                 }
-            }).bind(this, i));
+            }).bind(this, i);
+
         }
     };
     proto.startHandlingImages = function(){
         var len2 = this.img.length;
         var i2 = -1;
+
         while(i2++ < len2-1){
-            this.orig[i2] = {w:$(this.img[i2]).width(), h:$(this.img[i2]).height()};
-            $(this.img[i2]).css({
-                "background-image":"url('"+$(this.img[i2]).attr("data-src")+"')",
-                "background-position":"50% 0",
-                "background-repeat":"no-repeat",
-                "background-attachment":core.browser.isTouch ? "static" : "fixed"
-            });
+            this.orig[i2] = {w:core.rect(this.img[i2]).width, h:core.rect(this.img[i2]).height};
+            this.img[i2].style.backgroundImage = "url('"+this.img[i2].getAttribute("data-src")+"')";
+            this.img[i2].style.backgroundPosition = "50% 0";
+            this.img[i2].style.backgroundRepeat = "no-repeat";
+            this.img[i2].style.backgroundAttachment = core.browser.touch ? "static" : "fixed";
         }
-        $(window).on("resize", this._("onWindowResize"));
-        $(window).on("scroll", this._("onWindowScroll"));
-        $(window).trigger("resize");
+        window.onresize = this._("onWindowResize");
+        window.onscroll = this._("onWindowScroll");
+        this.onWindowResize();
 
     };
 
     proto.initialized = function(opts){
-        this.img = this.el.find("[img-dummy]");
+        this.img = this.find("[parallax]");
+        if(!this.img.length){
+            if(this.el.getAttribute("data-src")){
+                //set to self
+            }
+        }
         this.setParentSize();
         this.offset = 200;
         this.orig = [];
         this.loadImages();
     };
     proto.setParentSize = function(){
-        this.parent = {size:{width:this.el.width(), height:this.el.height()}}
+
+        this.parent = {size:{width:core.rect(this.el).width, height:core.rect(this.el).height}};
+
     };
     proto.onWindowScroll = function(evt){
-        if(core.browser.isTouch) return;
+        if(core.browser.touch) return;
+
         var len = this.img.length;
         while (len--) {
             var offset = this.img[len].getAttribute("scroll-offset") || .1;
             var top = core.rect(this.img[len]).top;
-            $(this.img[len]).css({"background-position":"center "+(top * offset)+"px"});
+            this.img[len].style.backgroundPosition = "center "+(top * offset)+"px";
         }
     };
     proto.onWindowResize = function(evt){
         this.setParentSize();
-        var w = $(window).width();
-        var h = $(window).height();
+        var w = window.innerWidth;
+        var h = window.innerHeight;
         this.stage = {w:w, h:h};
         var len = this.img.length;
         var i = -1;
@@ -95,89 +100,81 @@
 
         while(i++ < len-1){
 
-            if($(window).width() <= this.midW && $(window).width() > this.smallW && $(this.img[i]).attr("data-src-medium")){
+            if(w <= this.midW && w > this.smallW && this.img[i].getAttribute("data-src-medium")){
 
-                fakes[i] = $("<img/>").css({
-                    position:"absolute",
-                    left:0,
-                    top:0,
-                    visibility:"hidden"
+                    fakes[i] = document.createElement("img");
+                    fakes[i].style.position = "absolute";
+                    fakes[i].style.left = fakes[i].style.top = 0+"px";
+                    fakes[i].style.visibility = "hidden";
+                    document.body.appendChild(fakes[i]);
+                    fakes[i].setAttribute("src", this.img[i].getAttribute("data-src-medium"));
 
-                });
-                $("body").append(fakes[i]);
-                fakes[i].attr("src", $(this.img[i]).attr("data-src-medium"));
-                fakes[i].on("load", (function(xx){
-                    $(this.img[xx]).width(fakes[xx].width());
-                    $(this.img[xx]).height(fakes[xx].height());
-                    fakes[xx].remove();
-                    $(this.img[xx]).attr("data-src-medium-loaded", 1);
-                    this.orig[xx] = {w:$(this.img[xx]).width(), h:$(this.img[xx]).height()};
+                fakes[i].onload = (function(xx){
+                    this.img[xx].style.width = core.rect(fakes[xx]).width+"px";
+                    this.img[xx].style.height = core.rect(fakes[xx]).height+"px";
+                    document.body.removeChild(fakes[xx]);
+                    this.img[xx].setAttribute("data-src-medium-loaded", 1);
+                    this.orig[xx] = {w:core.rect(this.img[xx]).width, h:core.rect(this.img[xx]).width};
 
-                    $(this.img[xx]).css({
-                        "background-image":"url('"+$(this.img[xx]).attr("data-src-medium")+"')",
-                        "background-position":"50% 0",
-                        "background-repeat":"no-repeat",
-                        "background-attachment":core.browser.isTouch ? "static" : "fixed"
-                    });
+
+                    this.img[xx].style.backgroundImage = "url('"+this.img[xx].getAttribute("data-src-medium")+"')";
+                    this.img[xx].style.backgroundPosition = "50% 0",
+                    this.img[xx].style.backgroundRepeat = "no-repeat",
+                    this.img[xx].style.backgroundAttachment = core.browser.touch ? "static" : "fixed"
 
                     var aw = this.aspectScaleW(this.orig[xx].w, this.orig[xx].h, this.stage.w+this.offset);
                     if(aw.height < this.parent.size.height){
                         aw = this.aspectScaleH(this.orig[xx].w, this.orig[xx].h, this.parent.size.height+this.offset);
                     }
-                    $(this.img[xx]).width(aw.width);
-                    $(this.img[xx]).height(aw.height);
+                    this.img[xx].style.width = aw.width+"px";
+                    this.img[xx].style.height = aw.height+"px";
                     this.checkOverflow();
-                }).bind(this, i));
-            }else if($(window).width() <= this.smallW && $(this.img[i]).attr("data-src-small")){
+                }).bind(this, i);
 
-                fakes[i] = $("<img/>").css({
-                    position:"absolute",
-                    left:0,
-                    top:0,
-                    visibility:"hidden"
+            }else if(w <= this.smallW && this.img[i].getAttribute("data-src-small")){
 
-                });
-                $("body").append(fakes[i]);
-                fakes[i].attr("src", $(this.img[i]).attr("data-src-small"));
-                fakes[i].on("load", (function(xx){
-                    $(this.img[xx]).width(fakes[xx].width());
-                    $(this.img[xx]).height(fakes[xx].height());
-                    fakes[xx].remove();
-                    $(this.img[xx]).attr("data-src-small-loaded", 1);
-                    this.orig[xx] = {w:$(this.img[xx]).width(), h:$(this.img[xx]).height()};
+                fakes[i] = document.createElement("img");
+                fakes[i].style.position = "absolute";
+                fakes[i].style.left = fakes[i].style.top = 0+"px";
+                fakes[i].style.visibility = "hidden";
+                document.body.appendChild(fakes[i]);
+                fakes[i].setAttribute("src", this.img[i].getAttribute("data-src-small"));
 
-                    $(this.img[xx]).css({
-                        "background-image":"url('"+$(this.img[xx]).attr("data-src-small")+"')",
-                        "background-position":"50% 0",
-                        "background-repeat":"no-repeat",
-                        "background-attachment":core.browser.isTouch ? "static" : "fixed"
-                    });
+                fakes[i].onload = (function(xx){
+                    this.img[xx].style.width = core.rect(fakes[xx]).width+"px";
+                    this.img[xx].style.height = core.rect(fakes[xx]).height+"px";
+                    document.body.removeChild(fakes[xx]);
+                    this.img[xx].setAttribute("data-src-small-loaded", 1);
+                    this.orig[xx] = {w:core.rect(this.img[xx]).width, h:core.rect(this.img[xx]).width};
+
+
+
+                    this.img[xx].style.backgroundImage = "url('"+this.img[xx].getAttribute("data-src-small")+"')";
+                    this.img[xx].style.backgroundPosition = "50% 0",
+                    this.img[xx].style.backgroundRepeat = "no-repeat",
+                    this.img[xx].style.backgroundAttachment = core.browser.touch ? "static" : "fixed"
 
                     var aw = this.aspectScaleW(this.orig[xx].w, this.orig[xx].h, this.stage.w+this.offset);
                     if(aw.height < this.parent.size.height){
                         aw = this.aspectScaleH(this.orig[xx].w, this.orig[xx].h, this.parent.size.height+this.offset);
                     }
-                    $(this.img[xx]).width(aw.width);
-                    $(this.img[xx]).height(aw.height);
+                    this.img[xx].style.width = aw.width+"px";
+                    this.img[xx].style.height = aw.height+"px";
                     this.checkOverflow();
-                }).bind(this, i));
+                }).bind(this, i);
             }else{
-                if($(this.img[i]).attr("data-src-medium") || $(this.img[i]).attr("data-src-small")){
-                    $(this.img[i]).css({
-                        "background-image":"url('"+$(this.img[i]).attr("data-src")+"')",
-                        "background-position":"50% 0",
-                        "background-repeat":"no-repeat",
-                        "background-attachment":core.browser.isTouch ? "static" : "fixed"
-                    });
+                if(this.img[i].getAttribute("data-src-medium") || this.img[i].getAttribute("data-src-small")){
+                    this.img[i].style.backgroundImage = "url('"+this.img[i].getAttribute("data-src")+"')";
+                    this.img[i].style.backgroundPosition = "50% 0",
+                    this.img[i].style.backgroundRepeat = "no-repeat",
+                    this.img[i].style.backgroundAttachment = core.browser.touch ? "static" : "fixed"
                 }
-
-
                 var aw = this.aspectScaleW(this.orig[i].w, this.orig[i].h, this.stage.w+this.offset);
                 if(aw.height < this.parent.size.height){
                     aw = this.aspectScaleH(this.orig[i].w, this.orig[i].h, this.parent.size.height+this.offset);
                 }
-                $(this.img[i]).width(aw.width);
-                $(this.img[i]).height(aw.height);
+                this.img[i].style.width = aw.width+"px";
+                this.img[i].style.height = aw.height+"px";
                 this.checkOverflow();
             }
 
@@ -191,18 +188,16 @@
         var mleft = 0;
         var mtop = 0;
         while(len--){
-            mtop = pbounds.height/2 - $(this.img[len]).height()/2;
-            mleft = pbounds.width/2-$(this.img[len]).width()/2;
-            $(this.img[len]).css({
-                "background-size":$(this.img[len]).width()+"px "+$(this.img[len]).height()+"px",
-                "left":mleft,
-                "top":mtop
-            });
+            mtop = pbounds.height/2 - core.rect(this.img[len]).height/2;
+            mleft = pbounds.width/2-core.rect(this.img[len]).width/2;
+            this.img[len].style.backgroundSize = core.rect(this.img[len]).width+"px "+core.rect(this.img[len]).height;
+            this.img[len].style.left = mleft+"px";
+            this.img[len].style.top = mtop+"px";
 
         }
-        setTimeout(function(){
-            $(window).trigger("scroll");
-        }, 100);
+        setTimeout((function(){
+            this.onWindowScroll();
+        }).bind(this), 100);
 
 
     };
@@ -210,6 +205,7 @@
         return {height:th, width:(th/oh)*ow};
     };
     proto.aspectScaleW = function(ow, oh, tw){
+
         return {height:(tw/ow)*oh, width:tw};
     };
     core.registerNamespace("window.ResponsiveParallaxBG", ResponsiveParallaxBG);
