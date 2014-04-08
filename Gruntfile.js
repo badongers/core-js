15da6c26eb2fbf40d9ea843d73b45aada11de3fe
badongers <badongers@gmail.com>
2014-04-08 15:16:00 +1000

    Initial build

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,32 +7,26 @@ module.exports = function(grunt) {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
-
-                my_target:{
-                    files:{
-                        'dist/<%= pkg.name %>.min.js':[
-                            'src/core.js',
-                            'src/events/signal.js',
-                            'src/events/eventchannel.js',
-                            'src/net/xhr.js',
-                            'src/delegates/document.js',
-                            'src/delegates/module.js',
-                            'src/utils/raf.js'
-                        ],
-                        'dist/<%= pkg.name %>.plugins.min.js':[
-                            'addons/**/*.js'
-                        ]
-                    }
+            my_target:{
+                files:{
+                    'dist/<%= pkg.name %>.min.js':[
+                        'src/core.js',
+                        'src/events/signal.js',
+                        'src/events/eventchannel.js',
+                        'src/net/xhr.js',
+                        'src/delegates/document.js',
+                        'src/delegates/module.js',
+                        'src/utils/raf.js'
+                    ],
+                    'dist/<%= pkg.name %>.plugins.min.js':[
+                        'addons/{*,*/}/*.js'
+                    ]
                 }
-
+            }
         }
 
     });
-    // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
-
-    // Default task(s).
     grunt.registerTask('build', ['uglify']);
 
 
