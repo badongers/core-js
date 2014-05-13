e52e2dd9d42cddc1be9439cc5cd96da039867509
badongers <badongers@gmail.com>
2014-05-14 09:09:19 +1000

    Added geolocation services.

--- dist/core.plugins.src.js
+++ dist/core.plugins.src.js
@@ -1,4 +1,4 @@
-/*! core 2014-05-13 */
+/*! core 2014-05-14 */
 /**
  * The base module for the Core JS framework.
  * It provides helper methods for implementing OOP methodologies and basic utilities such as browser detection.
