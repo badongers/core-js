c33dbbe256a01cf8a06a26d03fd8feb95d63389a
badongers <badongers@gmail.com>
2014-05-14 10:58:30 +1000

    Generated documentation added.

---
+++ documentation/api.js
@@ -0,0 +1,36 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Function",
+        "core",
+        "core.Core",
+        "core.addons.CoreLocation",
+        "core.addons.CoreWindow",
+        "core.addons.uiscroll.CoreParallax",
+        "core.addons.uiscroll.CoreSnap",
+        "core.addons.webapp.LocalStorage",
+        "core.events.EventBroadcaster",
+        "core.events.EventDispatcher",
+        "core.net.XHR",
+        "core.wirings.Document",
+        "core.wirings.Module",
+        "core.wirings.OfflineModule"
+    ],
+    "modules": [
+        "addons",
+        "core"
+    ],
+    "allModules": [
+        {
+            "displayName": "addons",
+            "name": "addons",
+            "description": "The base module for the Core JS framework.\nIt provides helper methods for implementing OOP methodologies and basic utilities such as browser detection."
+        },
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "The base module for the Core JS framework.\nIt provides helper methods for implementing OOP methodologies and basic utilities such as browser detection."
+        }
+    ]
+} };
+});
